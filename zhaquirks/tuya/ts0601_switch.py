328e28521cd753b415d6aa86cfd1e4535a2041cb
Raza Hussain Akbari <rhakbari.96@gmail.com>
2024-12-05 17:42:25 +0500

    added support for 8 gang touch switch

--- zhaquirks/tuya/ts0601_switch.py
+++ zhaquirks/tuya/ts0601_switch.py
@@ -918,6 +918,7 @@ class TuyaSwitchX8_GP(TuyaSwitch):
     signature = {
         MODELS_INFO: [
             ("_TZE200_wktrysab", "TS0601"),
+            ("_TZE204_wktrysab", "TS0601"),
             ("TZE204_wktrysab", "TS0601"),
         ],
         ENDPOINTS: {
